9ed8e5241697cfb9b5d7a39de112044d477f9153
Chandrakala <indhireddy.chandrakala@gmail.com>
2024-06-01 15:16:49 -0400

    task1&2

---
+++ src/main/Task2.java
@@ -0,0 +1,12 @@
+class Hello
+{
+public static void main(String[] args) {
+    
+    System.out.print("Hello world");
+    for (int i = 10; i <= 50; i++)
+     {
+       System.out.println(i);
+    }
+    
+}
+}
