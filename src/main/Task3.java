aa14462a563004460d8e0cd93b561df47d351042
Chandrakala <indhireddy.chandrakala@gmail.com>
2024-06-01 16:06:05 -0400

    Task1&2&3

---
+++ src/main/Task3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ class Main
+ {
+
+public static void main(String[] args) {
+     Scanner obj = new Scanner(System.in);
+     
+
+     String input = obj.next();
+     System.out.println(input);
+     int number = Integer.parseInt(input); 
+     
+
+       if (number > 0)
+        {
+          System.out.println(number+" is positive."); 
+       }
+       else if (number < 0)
+        {
+           System.out.println(number+" is negative.");
+       }
+     {
+      
+    }
+    
+}
+}
